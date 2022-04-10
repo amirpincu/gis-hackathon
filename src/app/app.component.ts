3e076c6c1794d30d1dabc5fbc5a03b61b0a278f6
Amir <amir.pincu@gmail.com>
2022-04-10 14:23:03 +0300

    map added, shows streets, centers tel-aviv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import esriConfig from "@arcgis/core/config";
+import Map from "@arcgis/core/Map";
+import MapView from '@arcgis/core/views/MapView';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hazard-display';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+    esriConfig.apiKey = "AAPK3f4ddac3215b48719ab5bcdd1bcee174W-tRNmiA8_4fQYcKraekgDPubO4J1ld-smyOMyVKuePXCGC552_uu4-KZOKW_PQI";
+    const map = new Map({
+      basemap: "arcgis-streets" // Basemap layer
+    });
+
+    const view = new MapView({
+      map: map,
+      center: [34.789770779256685, 32.08687166775767],
+      zoom: 12, // scale: 72223.819286
+      container: "viewDiv",
+      constraints: {
+        snapToZoom: false
+      }
+    });
+
+    view.on("click", (event) => {
+      console.log(view.zoom);
+      console.log(view.toMap(event));
+    })
+  }
 }
